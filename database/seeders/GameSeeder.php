6cea7a7aaa2b118a073173b8f32bd2c9cf19bb75
Andrade . <j0s3.90jr@gmail.com>
2026-05-30 23:24:36 +0200

    Fichero de configuracion 3

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -16,55 +16,11 @@ class GameSeeder extends Seeder
             'name' => 'Pokémon TCG',
             'slug' => 'pokemon'
         ]);
-
-        // 2. Creamos la Expansión (Asignada a Pokémon)
-        $baseSet = CardSet::create([
-            'game_id' => $pokemonGame->id,
-            'name' => 'Base Set',
-            'code' => 'BS',
-            'total_cards' => 102
-        ]);
-
-        // 3. Creamos las cartas con nuestra nueva columna JSON 'attributes'
-        CardTemplate::create([
-            'card_set_id' => $baseSet->id,
-            'name' => 'Charizard',
-            'card_number' => '4/102',
-            'rarity' => 'Rare Holo',
-            'image_url' => 'https://images.pokemontcg.io/base1/4_hires.png',
-            'attributes' => [
-                'hp' => 120,
-                'types' => ['Fire'],
-                'stage' => 'Stage 2',
-                'weakness' => 'Water'
-            ]
-        ]);
-
-        CardTemplate::create([
-            'card_set_id' => $baseSet->id,
-            'name' => 'Blastoise',
-            'card_number' => '2/102',
-            'rarity' => 'Rare Holo',
-            'image_url' => 'https://images.pokemontcg.io/base1/2_hires.png',
-            'attributes' => [
-                'hp' => 100,
-                'types' => ['Water'],
-                'stage' => 'Stage 2',
-                'weakness' => 'Lightning'
-            ]
-        ]);
         
         // ¡Bonus! Puedes añadir One Piece fácilmente para probar el futuro
         $onePieceGame = Game::create([
             'name' => 'One Piece Card Game',
             'slug' => 'one-piece'
-        ]);
-
-        CardSet::create([
-            'game_id' => $onePieceGame->id,
-            'name' => 'Romance Dawn',
-            'code' => 'OP-01',
-            'total_cards' => 121
         ]);
     }
 }
